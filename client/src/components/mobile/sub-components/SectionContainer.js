f453bb474fb0e2126a7810d787fd3897c1408cdf
Trevor B <tbland96@knights.ucf.edu>
2020-08-11 10:44:12 -0400

    Removed padding to prevent side-scroll (on mobile). Reorganized CSS to make it more coherent. Modified css to remove unneeded styling.

--- client/src/components/mobile/sub-components/SectionContainer.js
+++ client/src/components/mobile/sub-components/SectionContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 const SectionContainer = ({children}) => (
-    <Grid container item direction="column" justify="center" alignItems="center" className="section secondary-font">
+    <Grid container item direction="column" justify="center" alignItems="center" className="section primary-font">
         {children}
     </Grid>
 )
