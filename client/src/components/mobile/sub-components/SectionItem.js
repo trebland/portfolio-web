8dca79d1a5cb00f77f75879c140b3dbd0a7117ab
Trevor B <tbland96@knights.ucf.edu>
2020-08-11 13:37:21 -0400

    Technologies are now dynamically created and considered arrays. This is to ensure they do not overflow as they have been causing issues on the firefox browser.

--- client/src/components/mobile/sub-components/SectionItem.js
+++ client/src/components/mobile/sub-components/SectionItem.js
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core';
 
 const imageStyles = {
     coverStyle: {
+      maxWidth: '80%',
       maxHeight: '40vh',
       borderRadius: '20px',
     },
@@ -36,8 +37,8 @@ const textStyles = {
 // img: Image of the project to display
 // title: Title of the project
 // internalLink: Link to the project page (internal link)
-// contributors: List of names of those who contributed to the project
-// technologies: List of technology icons that the project is composed of
+// contributors: List of names of those who contributed to the project (must be array type)
+// technologies: List of technology icons that the project is composed of (must be array type)
 // externalLinks: List of external links, linking to live project locations
 // date: date the project was released
 
@@ -69,7 +70,7 @@ const SectionItem = ({img, title, contributors, technologies, liveLinks, sourceL
 // title: Title of the project
 // contributors: List of names of those who contributed to the project (must be array type)
 // date: date the project was released
-// technologies: List of technology icons that the project is composed of (optional)
+// technologies: List of technology icons that the project is composed of (optional) (must be array type)
 // liveLinks: List of external links to live project locations (optional)
 // sourceLink: Github link (or other code repository link) (optional)
 // position: Whether the content should appear on the left or right side of the page
@@ -78,7 +79,7 @@ const SectionDescription = ({title, contributors, technologies, liveLinks, sourc
   <Grid className="section-description tertiary-color" style={textStyles.descriptionContentOverview} container item direction="column" justify="center" alignItems="center" xs={12}>         
     <div className="title" style={textStyles.descriptionTitle}>{title}</div>
     <div style={textStyles.descriptionContent}><CreditContainer>{contributors.map(contributor => (<div className="contributor" key={contributor}>{contributor}</div>))}</CreditContainer></div>
-    <div style={textStyles.descriptionContent}>{technologies ? <TechnologyContainer>{technologies}</TechnologyContainer> : <></>}</div>
+    <div style={textStyles.descriptionContent}>{technologies ? <TechnologyContainer>{technologies.map(technology => (<div key={technology}>{technology}</div>))}</TechnologyContainer> : <></>}</div>
     <div style={textStyles.descriptionContent}><LinkAndSourceContainer position={position}>{liveLinks ? <LinkContainer>{liveLinks}</LinkContainer> : <></>}{sourceLink ? <SourceContainer>{sourceLink}</SourceContainer> : <></>}</LinkAndSourceContainer></div>
     <div style={textStyles.descriptionContent}><DateContainer>{date}</DateContainer></div>
   </Grid>
