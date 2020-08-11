4706a6698a667ae4e8158cd105cf250877c42dbb
Trevor B <tbland96@knights.ucf.edu>
2020-08-11 12:59:13 -0400

    Modified mobile section max-height

--- client/src/components/mobile/sub-components/SectionItem.js
+++ client/src/components/mobile/sub-components/SectionItem.js
@@ -3,8 +3,7 @@ import { Grid } from '@material-ui/core';
 
 const imageStyles = {
     coverStyle: {
-      maxWidth: '80%',
-      maxHeight: '50vh',
+      maxHeight: '40vh',
       borderRadius: '20px',
     },
     profile: {
